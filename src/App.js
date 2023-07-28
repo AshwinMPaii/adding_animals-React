c97c3d62e1d9048d6b94133543f10d078cfedca9
Ashwin M Pai <ashwinpai808@gmail.com>
2023-07-28 12:12:24 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import AnimalShow from "./AnimalShow"
+import { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return (animals[Math.floor(Math.random() * animals.length)]);
 }
 
-export default App;
+export default function App() {
+    let [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    }
+    // console.log(animal);
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    })
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add Animal</button>
+            <div className="animal-list">{renderedAnimals}</div>
+        </div>
+    )
+}
